fc2cc6d01922e2c6bd9cc355779bd2c0e8a4cbc4
Eric T Dawson <eric.t.dawson@gmail.com>
2017-03-10 16:35:24 -0500

    Fixed tiny_sim

--- tiny_sim.sh
+++ tiny_sim.sh
@@ -4,15 +4,15 @@ python descrip_to_vcf.py tiny_descrip > tiny.vcf && \
 gsed -i "s/Random/tiny/g" mod.tiny.fa && \
 gsed -i "s/Random/tiny/g" tiny.fa && \
 gsed -i "s/Random/tiny/g" tiny.vcf && \
-head -n 2 mod.tiny.fa > tiny.mod.fa && \
-../art_bin_MountRainier/art_illumina -ss HS25 -p -na -i tiny.mod.fa -f 10 -l 125 -m 700 -s 50 -o tiny.alt && \
+head -n 2 mod.tiny.fa > tiny.mod.single.fa && \
+../art_bin_MountRainier/art_illumina -ss HS25 -p -na -i tiny.mod.single.fa -f 10 -l 125 -m 700 -s 50 -o tiny.alt && \
 ../art_bin_MountRainier/art_illumina -ss HS25 -p -na -i tiny.fa -f 10 -l 125 -m 700 -s 50 -o tiny.ref && \
 rm *.fai *.gam && \
 ../vg/bin/vg construct -r tiny.fa -v tiny.vcf -S -I mod.tiny.fa -a -f -m 32 > tiny.vg && \
 ../vg/bin/vg index -x tiny.xg -g tiny.gcsa tiny.vg -k 11 && \
 echo "Beginning mapping" && \
-time ../vg/bin/vg map -t 4 -f alt1.fq -f alt2.fq -x tiny.xg -g tiny.gcsa > alt.gam && \
-time ../vg/bin/vg map -t 4 -f ref1.fq -f ref2.fq -x tiny.xg -g tiny.gcsa > ref.gam && \
+time ../vg/bin/vg map -t 4 -f tiny.alt1.fq -f tiny.alt2.fq -x tiny.xg -g tiny.gcsa > alt.gam && \
+time ../vg/bin/vg map -t 4 -f tiny.ref1.fq -f tiny.ref2.fq -x tiny.xg -g tiny.gcsa > ref.gam && \
 cat ref.gam alt.gam > het.gam && \
 time ../vg/bin/vg genotype -F tiny.fa -I mod.tiny.fa -V tiny.vcf -G alt.gam tiny.vg x > recall.alt.vcf && \
 cat recall.alt.vcf
